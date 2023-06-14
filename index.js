19aef736287d879650c15a79571dbdf61d0929fd
sakib21103090 <21103090@iubat.edu>
2023-06-14 22:12:38 +0600

    add jwt

--- index.js
+++ index.js
@@ -220,20 +220,20 @@ app.delete('/users/:id', async (req, res) => {
 
     })
 
-    // create payment intent
-    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
-      const { price } = req.body;
-      const amount = parseInt(price * 100);
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount,
-        currency: 'usd',
-        payment_method_types: ['card']
-      });
+    // // create payment intent
+    // app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+    //   const { price } = req.body;
+    //   const amount = parseInt(price * 100);
+    //   const paymentIntent = await stripe.paymentIntents.create({
+    //     amount: amount,
+    //     currency: 'usd',
+    //     payment_method_types: ['card']
+    //   });
 
-     return res.send({
-        clientSecret: paymentIntent.client_secret
-      })
-    })
+    //  return res.send({
+    //     clientSecret: paymentIntent.client_secret
+    //   })
+    // })
 
 
 
